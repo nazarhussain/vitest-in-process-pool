69f81de1093662b932948e4e918d2e6ba4982c34
Nazar Hussain <nazarhussain@gmail.com>
2025-02-24 13:55:12 +0100

    Update the package deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["test/unit/**/*.test.ts"],
-    pool: "src/index.ts"
+    pool: "src/index.ts",
+    reporters: [["default", {"summary": false}]]
   }
 });
