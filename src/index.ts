095f8a371bd155f41602a5457bbe2d9d041c422b
Nazar Hussain <nazarhussain@gmail.com>
2025-05-15 14:26:47 +0200

    Refactor the pool to work with latest version of vitest

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,168 @@
-import {
+import type {
   RuntimeRPC,
   WorkerGlobalState,
   ContextTestEnvironment,
   ContextRPC,
   CancelReason,
+  SerializedConfig,
 } from "vitest";
 import {
   createMethodsRPC,
-  WorkspaceProject,
+  type WorkspaceProject,
   type ProcessPool,
   type TestSpecification,
   type Vitest,
+  type TestProject,
+  type RunnerTask,
 } from "vitest/node";
+import type { Awaitable } from "vitest/utils";
 import { runBaseTests } from "vitest/workers";
 import { createStackString, parseStacktrace } from "vitest/utils";
 import {
+  getConfig,
   getUniqueProjects,
+  groupBy,
   groupFilesByEnv,
-  groupFilesByProject,
   loadEnvironment,
 } from "./utils.js";
 import { setupInspect } from "./inspector.js";
+
+export type RunWithFiles = (
+  files: TestSpecification[],
+  invalidates?: string[]
+) => Awaitable<void>;
+
+interface FileSpecification {
+  filepath: string;
+  testLocations: number[] | undefined;
+}
 
 export default function createInProcessPool(
   ctx: Vitest,
   _opts: { execArgv: string[]; env: Record<string, unknown> }
 ): ProcessPool {
+  const originalUpdateId = ctx.state.updateId;
+
+  const runWithFiles = (name: "run" | "collect"): RunWithFiles => {
+    let id = 0;
+
+    if (name === "run") {
+      ctx.state.updateId = (task: RunnerTask, project: TestProject) => {
+        ctx.state.idMap.delete(task.id);
+        originalUpdateId.call(ctx.state, task, project);
+      };
+    }
+
+    async function runFiles(
+      project: TestProject,
+      config: SerializedConfig,
+      files: FileSpecification[],
+      environment: ContextTestEnvironment,
+      invalidates: string[] = []
+    ) {
+      const prepareStart = Date.now();
+      const paths = files.map((f) => f.filepath);
+      // ctx.state.clearFiles(project, paths);
+
+      const workerId = ++id;
+      const rpcContext = {
+        pool: "in-process-pool",
+        worker: "single-worker",
+        workerId,
+        config,
+        files,
+        projectName: project.name,
+        environment,
+        providedContext: project.getProvidedContext(),
+        invalidates,
+      } satisfies ContextRPC;
+
+      const cleanupInspect = setupInspect(rpcContext);
+
+      let setCancel = (_reason: CancelReason) => {};
+      const onCancel = new Promise<CancelReason>((resolve) => {
+        setCancel = resolve;
+      });
+
+      try {
+        const rpc = projectRpcs.get(project);
+        const beforeEnvironmentTime = Date.now();
+        const environment = await loadEnvironment(rpcContext);
+
+        if (rpcContext.environment.transformMode) {
+          environment.transformMode = rpcContext.environment.transformMode;
+        }
+
+        const state = {
+          ctx: rpcContext,
+          // This cache map is replaced in `runBaseTests` anyway so we don't need it here
+          moduleCache: {} as WorkerGlobalState["moduleCache"],
+          moduleExecutionInfo: new Map(),
+          config: rpcContext.config,
+          onCancel,
+          environment,
+          durations: {
+            environment: beforeEnvironmentTime,
+            prepare: prepareStart,
+          },
+          rpc: rpc as unknown as WorkerRpc,
+          providedContext: project.getProvidedContext(),
+          onFilterStackTrace(stack) {
+            return createStackString(parseStacktrace(stack));
+          },
+        } satisfies WorkerGlobalState;
+
+        await runBaseTests(name, state);
+      } catch (error) {
+        setCancel("test-failure");
+        throw error;
+      } finally {
+        cleanupInspect();
+      }
+    }
+
+    return async (specs, invalidates) => {
+      const projects = getUniqueProjects(specs);
+      for (const project of projects) {
+        projectRpcs.set(project, createMethodsRPC(project));
+      }
+
+      const filesByEnv = await groupFilesByEnv(specs);
+      const envs = envsOrder.concat(
+        Object.keys(filesByEnv).filter((env) => !envsOrder.includes(env))
+      );
+
+      for (const env of envs) {
+        const envFiles = filesByEnv[env];
+
+        if (!envFiles?.length) {
+          continue;
+        }
+
+        const filesByOptions = groupBy(
+          envFiles,
+          ({ project, environment }) =>
+            project.name + JSON.stringify(environment.options)
+        );
+
+        for (const files of Object.values(filesByOptions)) {
+          const fileSpecs: FileSpecification[] = files.map((f) => ({
+            filepath: f.file.filepath,
+            testLocations: f.file.testLocations,
+          }));
+
+          await runFiles(
+            files[0].project,
+            getConfig(files[0].project),
+            fileSpecs,
+            files[0].environment,
+            invalidates
+          );
+        }
+      }
+    };
+  };
+
   return {
     name: "in-process-pool",
     async runTests(
@@ -35,9 +172,8 @@ export default function createInProcessPool(
       // TODO: don't rely on reassigning process.exit
       // https://github.com/vitest-dev/vitest/pull/4441#discussion_r1443771486
       const exit = process.exit;
-
       try {
-        await execute({ name: "run", specs, invalidates });
+        await runWithFiles("run")(specs, invalidates);
       } finally {
         process.exit = exit;
       }
@@ -49,119 +185,18 @@ export default function createInProcessPool(
       // TODO: don't rely on reassigning process.exit
       // https://github.com/vitest-dev/vitest/pull/4441#discussion_r1443771486
       const exit = process.exit;
-
       try {
-        await execute({ name: "collect", specs, invalidates });
+        await runWithFiles("collect")(specs, invalidates);
       } finally {
         process.exit = exit;
       }
     },
-    async close(): Promise<void> {},
+    async close(): Promise<void> {
+      ctx.logger.log("Closing in-process pool");
+    },
   };
 }
 
 const projectRpcs = new WeakMap<WorkspaceProject, RuntimeRPC>();
 const envsOrder = ["node", "jsdom", "happy-dom", "edge-runtime"];
 type WorkerRpc = WorkerGlobalState["rpc"];
-
-async function execute({
-  name,
-  specs,
-  invalidates,
-}: {
-  name: "run" | "collect";
-  specs: TestSpecification[];
-  invalidates?: string[];
-}) {
-  const projects = getUniqueProjects(specs);
-  for (const project of projects) {
-    projectRpcs.set(project, createMethodsRPC(project));
-  }
-
-  const filesByEnv = await groupFilesByEnv(specs);
-  const envs = envsOrder.concat(
-    Object.keys(filesByEnv).filter((env) => !envsOrder.includes(env))
-  );
-
-  for (const env of envs) {
-    const envFiles = filesByEnv[env];
-    if (!envFiles?.length) continue;
-
-    const filesByProjects = await groupFilesByProject(envFiles);
-
-    for (const p of Object.keys(filesByProjects)) {
-      const files = filesByProjects[p];
-
-      await executeFiles({
-        name,
-        project: files[0].project,
-        environment: envFiles[0].environment,
-        specs: files,
-        invalidates,
-      });
-    }
-  }
-}
-
-async function executeFiles({
-  project,
-  specs,
-  name,
-  environment,
-  invalidates
-}: {
-  name: "run" | "collect";
-  project: WorkspaceProject;
-  specs: TestSpecification[];
-  environment: ContextTestEnvironment;
-  invalidates?: string[];
-}) {
-  project.vitest.state.clearFiles(project, specs.map(s => s.moduleId));
-  const context = {
-    pool: "in-process-pool",
-    worker: "single-worker",
-    workerId: 10,
-    config: project.serializedConfig,
-    files: specs.map(s => s.moduleId),
-    projectName: project.name,
-    environment,
-    providedContext: project.getProvidedContext(),
-    invalidates,
-  } satisfies ContextRPC;
-
-  const cleanupInspect = setupInspect(context);
-
-  let setCancel = (_reason: CancelReason) => {};
-  const onCancel = new Promise<CancelReason>((resolve) => {
-    setCancel = resolve;
-  });
-
-  try {
-    const rpc = projectRpcs.get(project)!;
-
-    const state = {
-      ctx: context,
-      // This cache map is replaced in `runBaseTests` anyway so we don't need it here       
-      moduleCache: {} as WorkerGlobalState['moduleCache'],
-      moduleExecutionInfo: new Map(),
-      config: context.config,
-      onCancel,
-      environment: loadEnvironment(context),
-      durations: {
-        environment: 0,
-        prepare: 0,
-      },
-      rpc: rpc as unknown as WorkerRpc,
-      providedContext: project.getProvidedContext(),
-      onFilterStackTrace(stack) {
-        return createStackString(parseStacktrace(stack));
-      },
-    } satisfies WorkerGlobalState;
-
-    await runBaseTests(name, state);
-  } catch (err) {
-    setCancel("test-failure");
-  } finally {
-    cleanupInspect();
-  }
-}
